5dd49e3659c50d9d281727151f0b0db5608fa76e
Chad Eddington <cdavideddington@gmail.com>
2014-07-14 12:50:37 -0600

    added a second button

--- TheBasics/src/com/chadeddington/thebasics/MainActivity.java
+++ TheBasics/src/com/chadeddington/thebasics/MainActivity.java
@@ -7,12 +7,18 @@ import android.app.Activity;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		//uses Fullscreen
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		
 		setContentView(R.layout.splash_menu);
 		
