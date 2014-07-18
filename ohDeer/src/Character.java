b8614ec88fedce48cce243c4087fdcc0c8214e50
Chad Eddington <cdavideddington@gmail.com>
2014-07-18 12:48:09 -0600

    The Final Presentation Version

---
+++ ohDeer/src/Character.java
@@ -0,0 +1,21 @@
+import com.gorilla.ohdeer.R;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+
+
+public class Character{
+	Bitmap linkLeft; 
+	Bitmap linkRight;
+	
+	
+	public void myDraw(Canvas canvas) {
+		// TODO 
+	//	update();
+		
+
+	}
+
+}
